cf6df0fab6e825a4e51095c36191258de67f1f61
Jenslee Dsouza <dsouzajenslee@gmail.com>
2025-12-21 13:40:14 +0530

    cues updated

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sys
 import time
 
 import RPi.GPIO as GPIO
+
 # from EmulatorGUI import GPIO
 
 # ===================== CONFIG =====================
@@ -19,16 +20,36 @@ ACTIVE_LOW = True
 # Audio file (WAV recommended for accurate timing)
 AUDIO_FILE = "monologue.wav"
 
+
+LIGHT_1_PIN = 21  # GPIO21 (pin 40)
+LIGHT_2_PIN = 20  # GPIO20 (pin 38)
+LIGHT_3_PIN = 16  # GPIO16 (pin 36)
+LIGHT_4_PIN = 12  # GPIO12 (pin 32)
+LIGHT_5_PIN = 26  # GPIO26 (pin 37)
+LIGHT_6_PIN = 19  # GPIO19 (pin 35)
+LIGHT_7_PIN = 13  # GPIO13 (pin 33)
+LIGHT_8_PIN = 6  # GPIO6  (pin 31)
+
 # Cue format:
 # (time_in_seconds_from_start, gpio_pin, state)
 # state: True = ON, False = OFF
 CUES = [
-    (0.0, 17, True),  # Light 1 ON immediately
-    (4.5, 17, False),  # Light 1 OFF
-    (4.5, 27, True),  # Light 2 ON
-    (9.8, 27, False),  # Light 2 OFF
-    (10.0, 22, True),  # Light 3 ON
-    (18.0, 22, False),  # Light 3 OFF
+    (0.0, LIGHT_1_PIN, True),  # Light 1 ON immediately
+    (1.0, LIGHT_1_PIN, False),  # Light 1 OFF
+    (1.1, LIGHT_2_PIN, True),  # Light 2 ON
+    (2.0, LIGHT_2_PIN, False),  # Light 2 OFF
+    (2.1, LIGHT_3_PIN, True),  # Light 3 ON
+    (3.0, LIGHT_3_PIN, False),  # Light 3 OFF
+    (3.1, LIGHT_4_PIN, True),  # Light 4 ON
+    (4.0, LIGHT_4_PIN, False),  # Light 4 OFF
+    (4.1, LIGHT_5_PIN, True),  # Light 5 ON
+    (5.0, LIGHT_5_PIN, False),  # Light 5 OFF
+    (5.1, LIGHT_6_PIN, True),  # Light 6 ON
+    (6.0, LIGHT_6_PIN, False),  # Light 6 OFF
+    (6.1, LIGHT_7_PIN, True),  # Light 7 ON
+    (7.0, LIGHT_7_PIN, False),  # Light 7 OFF
+    (7.1, LIGHT_8_PIN, True),  # Light 8 ON
+    (8.0, LIGHT_8_PIN, False),  # Light 8 OFF
 ]
 
 # ================================================
